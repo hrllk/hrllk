1dc537221c5f7be8c3963427f9273f0c8750e700
hrllk <roovcloudy@gmail.com>
2025-03-29 00:40:28 +0900

    update render posts

--- render_latest_posts.py
+++ render_latest_posts.py
@@ -7,14 +7,6 @@ RSS_FEED = feedparser.parse(URL)
 MAX_POST = 7
 
 markdown_text = """
-<!-- header -->
-<!-- ![header](https://capsule-render.vercel.app/api?type=transparent&color=auto&height=300&section=header&text=:\)&fontSize=90) -->
-<!-- Hi, 🙋🏻‍♂️ I'm backend developer <br> -->
-<!-- I love listening lofi <br> -->
-<!-- [![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fhrllk%2F&count_bg=%23D4E7F0&title_bg=%2378BBD8&icon=&icon_color=%23B8B8B8&title=hits&edge_flat=false)](https://hits.seeyoufarm.com) -->
-
-<!-- ## Hi There, I'm HwiRyung :) -->
-
 
 ## About Me
 Hello! My name is Hwiryung Kim, and I'm a dedicated backend developer with a passion for crafting clean, efficient code and solving complex problems. With a strong foundation in Java and expertise in the Spring framework, I thrive in architecting robust systems that stand the test of time. 
