42a64e5dabd2d47bed47916e440787e6e438bfa2
hrllk <roovcloudy@gmail.com>
2025-04-07 23:12:52 +0900

    update

--- render_latest_posts.py
+++ render_latest_posts.py
@@ -1,31 +1,31 @@
-
-import feedparser
-import time
-
-URL = "https://hrllk.github.io/feed.xml"
-RSS_FEED = feedparser.parse(URL)
-MAX_POST = 7
-
-markdown_text = """
-
-## About Me
-Hello! My name is Hwiryung Kim, and I'm a dedicated backend developer with a passion for crafting clean, efficient code and solving complex problems. With a strong foundation in Java and expertise in the Spring framework, I thrive in architecting robust systems that stand the test of time. 
-
-In addition, I have a keen interest in design patterns and their application to solving real-world problems. Whether it's implementing creational, structural, or behavioral patterns, I leverage these patterns to improve code readability, flexibility, and maintainability.
-
-Above all, I consider myself a problem solver at heart. I thrive on tackling challenges head-on, breaking them down into manageable tasks, and finding innovative solutions to overcome them. I'm always eager to learn new technologies and methodologies to expand my skill set and stay ahead in this dynamic field.
-
-##  Posts
-"""
-
-for idx, feed in enumerate(RSS_FEED['entries']):
-    if idx > MAX_POST:
-        break
-    else:
-        feed_date = feed['published_parsed']
-        markdown_text += f"[{time.strftime('%Y/%m/%d', feed_date)} - {
-            feed['title']}]({feed['link']}) <br/>\n"
-
-f = open("README.md", mode="w", encoding="utf-8")
-f.write(markdown_text)
-f.close()
+#
+# import feedparser
+# import time
+#
+# URL = "https://hrllk.github.io/feed.xml"
+# RSS_FEED = feedparser.parse(URL)
+# MAX_POST = 7
+#
+# markdown_text = """
+#
+# ## About Me
+# Hello! My name is Hwiryung Kim, and I'm a dedicated backend developer with a passion for crafting clean, efficient code and solving complex problems. With a strong foundation in Java and expertise in the Spring framework, I thrive in architecting robust systems that stand the test of time. 
+#
+# In addition, I have a keen interest in design patterns and their application to solving real-world problems. Whether it's implementing creational, structural, or behavioral patterns, I leverage these patterns to improve code readability, flexibility, and maintainability.
+#
+# Above all, I consider myself a problem solver at heart. I thrive on tackling challenges head-on, breaking them down into manageable tasks, and finding innovative solutions to overcome them. I'm always eager to learn new technologies and methodologies to expand my skill set and stay ahead in this dynamic field.
+#
+# ##  Posts
+# """
+#
+# for idx, feed in enumerate(RSS_FEED['entries']):
+#     if idx > MAX_POST:
+#         break
+#     else:
+#         feed_date = feed['published_parsed']
+#         markdown_text += f"[{time.strftime('%Y/%m/%d', feed_date)} - {
+#             feed['title']}]({feed['link']}) <br/>\n"
+#
+# f = open("README.md", mode="w", encoding="utf-8")
+# f.write(markdown_text)
+# f.close()
--- update_blog_post.py
+++ update_blog_post.py
@@ -1,24 +1,24 @@
-
-## update post of blog 
-import feedparser
-
-blog_url = "https://hrllk.github.io/feed.xml
-rss_feed = feedparser.parse(blog_url)
-
-MAX_NUM = 5
-latest_posts = ""
-
-for idx, entrie in enumerate(rss_feed['entries']):
-    if idx > MAX_NUM:
-        break;
-    feed_date = entrie['published_parsed']
-    latest_posts += f" - [{feed_date.tm_mon}/{feed_date.tm_mday} - {entrie['title']}]){entrie['link']})\n"
-    
-preREADME = """
-## 기존 README.md 내용 
-"""
-
-resultREADME = f"{preREADME}{latest_posts}"
-
-with open("README.md", "w", encoding='utf-8') as f :
-    f.write(resultREADME)
+#
+# ## update post of blog 
+# import feedparser
+#
+# blog_url = "https://hrllk.github.io/feed.xml
+# rss_feed = feedparser.parse(blog_url)
+#
+# MAX_NUM = 5
+# latest_posts = ""
+#
+# for idx, entrie in enumerate(rss_feed['entries']):
+#     if idx > MAX_NUM:
+#         break;
+#     feed_date = entrie['published_parsed']
+#     latest_posts += f" - [{feed_date.tm_mon}/{feed_date.tm_mday} - {entrie['title']}]){entrie['link']})\n"
+#     
+# preREADME = """
+# ## 기존 README.md 내용 
+# """
+#
+# resultREADME = f"{preREADME}{latest_posts}"
+#
+# with open("README.md", "w", encoding='utf-8') as f :
+#     f.write(resultREADME)
